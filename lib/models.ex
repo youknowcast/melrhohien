48aaf7c9f212c4958d9615cfbd63d4afd80d8be5
youknowcast <ykc@daycrift.net>
2021-01-15 03:35:56 +0900

    commit 漏れ

--- lib/models.ex
+++ lib/models.ex
@@ -7,16 +7,17 @@ defmodule Melrhohien.Models do
   alias Melrhohien.Models.ImageTag
   alias Melrhohien.Models.Tag
 
-  @offset 5
+  @limit 5
 
-  def get_file_name_and_tags_by_file_name do
+  def get_file_name_and_tags_by_file_name(offset \\ 0) do
     (from t in Tag,
       join: it in  ImageTag,
       on: t.id == it.tag_id,
       join: i in Image,
       on: i.id == it.image_id,
       select: [i.file_name, t.name],
-      offset: @offset
+      offset: ^offset,
+      limit: @limit,
       order_by: i.inserted_at)
       |> Repo.all
   end
