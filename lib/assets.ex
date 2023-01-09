d55f5f9bc7c12252e162198ac492c60f73f7f9f3
Ambe Hideki <ykc@daycrift.net>
2023-01-09 22:32:28 +0900

    fix: add assets

---
+++ lib/assets.ex
@@ -0,0 +1,11 @@
+defmodule Melrhohien.Assets do
+  use Scenic.Assets.Static,
+      otp_app: :melrhohien,
+      alias: [
+        bellface_logo: "images/bellface_logo.jpg",
+        computer_cloud_system: "images/computer_cloud_system.png",
+        document_sekkeisyo: "images/document_sekkeisyo.png",
+        document_siyousyo: "images/document_siyousyo.png",
+        gengou_system_reiwa: "images/gengou_system_reiwa.png",
+      ]
+end
